69d1536c8eb531474feaf1659eef6454928fc247
Davi Carreiro <carreirodavi3702gmail.com>
2025-04-07 11:01:52 -0300

    Criando Tabelas Models.

---
+++ Aplicação Web Simples com Flask/Meu_Projeto/app/models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+    estoque = db.Column(db.Integer, nullable=False)
