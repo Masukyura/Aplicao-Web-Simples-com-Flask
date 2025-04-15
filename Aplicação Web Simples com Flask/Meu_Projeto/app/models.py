be426a9243dfa78ddcde538eb1954fdfbfe75505
Wal-dreamer <great1one22@gmail.com>
2025-04-15 12:33:55 -0300

    atualização 1

--- Aplicação Web Simples com Flask/Meu_Projeto/app/models.py
+++ Aplicação Web Simples com Flask/Meu_Projeto/app/models.py
@@ -2,12 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Usuario(db.Model):
+class clientes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
-class Produto(db.Model):
+class produtos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     preco = db.Column(db.Float, nullable=False)
--- Aplicação Web Simples com Flask/Meu_Projeto/app/views.py
+++ Aplicação Web Simples com Flask/Meu_Projeto/app/views.py
@@ -1,8 +1,20 @@
 from main import app
 from flask import render_template
+from controllers import produto_controller, cliente_controller
 
+app = Blueprint('app', __name__)
 
 #Rotas
 @app.route("/")
-def base():
-    return render_template("base.html")
+def index():
+    return render_template("index.html")
+
+# Página de produtos (lista)
+@app.route('/produtos')
+def listar_produtos():
+    return produto_controller.listar_produtos()
+
+# Página de clientes (lista)
+@app.route('/clientes')
+def listar_clientes():
+    return cliente_controller.listar_clientes()
